32f0a947e49635de707d1af50989534ab5360d67
Daniel Redondo <danielredondosanchez@hotmail.com>
2022-03-28 14:47:32 +0200

    change encoding, remove options

--- knowseq_shiny.R
+++ knowseq_shiny.R
@@ -1,10 +1,7 @@
 library(shiny)
 library(BiocManager)
 
-source('ui.R', local = TRUE)
+source('ui.R', encoding = "UTF-8")
 source('server.R')
-
-options(repos = BiocManager::repositories())
-getOption("repos")
 
 shinyApp(ui = ui, server = server)
